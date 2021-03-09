9dfa16ea6ee7bf53db32ea67d30b5a4bcd54aa36
Dylan Viola <dylanviola@gmail.com>
2021-03-09 14:57:41 -0500

    add trailing / redirect

--- cra-template-craft-react/template/src/components/app/app.js
+++ cra-template-craft-react/template/src/components/app/app.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
 import Page from "components/page";
 
 const App = () => {
+  const { pathname } = useLocation();
   return (
     <div>
       <Switch>
+        <Redirect from="/:url*(/+)" to={pathname.slice(0, -1)} />
         <Route path="/" component={Page} />
       </Switch>
     </div>
